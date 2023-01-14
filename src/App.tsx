382f50b300b4f110addb2e8c84254c5efaadd88b
Denys Hleba <denis133445500gmail.com>
2023-01-14 17:44:49 +0200

    Initialize react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam,
+      placeat quam. Ut rem impedit repellat sequi voluptatum, eveniet maxime
+      excepturi consequuntur blanditiis asperiores deleniti eum cumque? Fuga
+      ullam nostrum neque.
+    </div>
+  );
+}
+
+export default App;
